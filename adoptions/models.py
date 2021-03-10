677f15b2bb576d13631c571eef5937651c7d403a
Taleb <alitk@vt.edu>
2021-03-10 15:28:30 -0500

    fixed some bugs

--- adoptions/models.py
+++ adoptions/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-    name = models.Charfield(max_length = 100)
-    submitter = models.Charfield(max_length = 100)
-    species = models.Charfield(max_length = 30)
-    breed = models.Charfield(max_length = 30, blank = True)
-    description = models.TxtField()
+    name = models.CharField(max_length = 100)
+    submitter = models.CharField(max_length = 100)
+    species = models.CharField(max_length = 30)
+    breed = models.CharField(max_length = 30, blank = True)
+    description = models.TextField()
     sex = models.CharField(max_length = 1, choices = SEX_CHOICES,
      blank=True)
     submission_date = models.DateTimeField()
